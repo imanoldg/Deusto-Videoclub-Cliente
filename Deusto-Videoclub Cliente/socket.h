04a1039577353686bf0972e08fbccd9be81e2b4c
imanoldg <ima.dominguez@opendeusto.es>
2024-05-16 13:42:07 +0200

    implementada libreria ws2_32 y metodos de socket

--- Deusto-Videoclub Cliente/socket.h
+++ Deusto-Videoclub Cliente/socket.h
@@ -4,10 +4,16 @@
  *  Created on: 16 may 2024
  *      Author: Usuario
  */
-
 #ifndef SOCKET_H_
 #define SOCKET_H_
 
+#include <winsock2.h>
+#include "Usuario.h"
 
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 6000
+
+int setUpSocket(SOCKET* s);
+int enviarComandoIniciarSesion(SOCKET* s, char* dni, char* contrasena, Usuario& u);
 
 #endif /* SOCKET_H_ */
