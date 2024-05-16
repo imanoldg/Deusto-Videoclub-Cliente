6c5b2b7a9900b868e615cc789b74b8095ddb3e6d
imanoldg <ima.dominguez@opendeusto.es>
2024-05-16 13:41:08 +0200

    clase usuario copiada

---
+++ Deusto-Videoclub Cliente/Usuario.cpp
@@ -0,0 +1,152 @@
+/*
+ * Usuario.cpp
+ *
+ *  Created on: 16 may 2024
+ *      Author: Usuario
+ */
+#include "Usuario.h"
+#include <iostream>
+#include "string.h"
+using namespace std;
+
+
+Usuario::Usuario(char* dni, char* nombre, char* apellido, char* email, int telefono, char* user, char* contra, char genero, char* fechaNcto, int numTarjeta, int puntos){
+	strcpy(this->dni, dni);
+	strcpy(this->nombre, nombre);
+	strcpy(this->apellido, apellido);
+	strcpy(this->email, email);
+	this->telefono = telefono;
+	strcpy(this->user, user);
+	strcpy(this->contra, contra);
+	this->genero = genero;
+	this->fechaNcto = new char(strlen(fechaNcto) + 1);
+	strcpy(this->fechaNcto, fechaNcto);
+	this->numTarjeta = numTarjeta;
+	this->puntos = puntos;
+}
+
+Usuario::Usuario(){
+	strcpy(this->dni, "");
+	strcpy(this->nombre, "");
+	strcpy(this->apellido, "");
+	strcpy(this->email, "");
+	this->telefono = 0;
+	strcpy(this->user, "");
+	strcpy(this->contra, "");
+	this->genero = 'M';
+	this->fechaNcto = new char(strlen("") + 1);
+	strcpy(this->fechaNcto, "");
+	this->numTarjeta = 0;
+	this->puntos = 0;
+}
+
+Usuario::Usuario(Usuario &otro){
+	strcpy(this->dni, otro.dni);
+	strcpy(this->nombre, otro.nombre);
+	strcpy(this->apellido, otro.apellido);
+	strcpy(this->email, otro.email);
+	this->telefono = otro.telefono;
+	strcpy(this->user, otro.user);
+	strcpy(this->contra, otro.contra);
+	this->genero = otro.genero;
+	this->fechaNcto = new char(strlen(otro.fechaNcto) + 1);
+	strcpy(this->fechaNcto, otro.fechaNcto);
+	this->numTarjeta = otro.numTarjeta;
+	this->puntos = otro.puntos;
+}
+
+Usuario::~Usuario(){
+
+}
+
+
+char* Usuario::getDNI(){
+	return this->dni;
+}
+
+char* Usuario::getNombre(){
+	return this->nombre;
+}
+
+char* Usuario::getApellido(){
+	return this->apellido;
+}
+
+char* Usuario::getEmail(){
+	return this->email;
+}
+
+char* Usuario::getUser(){
+	return this->user;
+}
+
+int Usuario::getTlf(){
+	return this->telefono;
+}
+
+char* Usuario::getContra(){
+	return this->contra;
+}
+
+char Usuario::getGenero(){
+	return this->genero;
+}
+
+char* Usuario::getFechaNcto(){
+	return this->fechaNcto;
+}
+
+int Usuario::getNumTarjeta(){
+	return this->numTarjeta;
+}
+
+int Usuario::getPuntos(){
+	return this->puntos;
+}
+
+void Usuario::setPuntos(int puntos){
+	this->puntos = puntos;
+}
+
+void Usuario::setApellido(char* apellido){
+	strcpy(this->apellido, apellido);
+}
+
+void Usuario::setContra(char* contra){
+	strcpy(this->contra, contra);
+}
+
+void Usuario::setDNI(char* dni){
+	strcpy(this->dni, dni);
+}
+
+void Usuario::setEmail(char* email){
+	strcpy(this->email, email);
+}
+
+void Usuario::setFechaNcto(char* fecha){
+	this->fechaNcto = new char(strlen(fecha) + 1);
+	strcpy(this->fechaNcto, fecha);
+}
+
+void Usuario::setGenero(char genero){
+	this->genero= genero;
+}
+
+void Usuario::setNombre(char* nombre){
+	strcpy(this->nombre, nombre);
+}
+
+void Usuario::setNumTarjeta(int numTarj){
+	this->numTarjeta = numTarj;
+}
+
+void Usuario::setTlf(int tlf){
+	this->telefono = tlf;
+}
+
+void Usuario::setUser(char* user){
+	strcpy(this->user, user);
+}
+
+
---
+++ Deusto-Videoclub Cliente/Usuario.h
@@ -0,0 +1,57 @@
+/*
+ * Usuario.h
+ *
+ *  Created on: 16 may 2024
+ *      Author: Usuario
+ */
+#ifndef USUARIO_H_
+#define USUARIO_H_
+
+class Usuario{
+private:
+	char dni[10];
+	char nombre[40];
+	char apellido[60];
+	char email[60];
+	int telefono;
+	char user[40];
+	char contra[16];
+	char genero;
+	char* fechaNcto;
+	int numTarjeta;
+	int puntos;
+
+public:
+	Usuario(char* dni, char* nombre, char* apellido, char* email, int telefono, char* user, char* contra, char genero, char* fechaNcto, int numTarjeta, int puntos);
+	Usuario();
+	Usuario(Usuario &u);
+	~Usuario();
+
+	char* getDNI();
+	char* getNombre();
+	char* getApellido();
+	char* getEmail();
+	int getTlf();
+	char* getUser();
+	char* getContra();
+	char getGenero();
+	char* getFechaNcto();
+	int getNumTarjeta();
+	int getPuntos();
+
+	void setPuntos(int puntos);
+	void setDNI(char* dni);
+	void setNombre(char* nombre);
+	void setApellido(char* apellido);
+	void setEmail(char* email);
+	void setTlf(int tlf);
+	void setUser(char* user);
+	void setContra(char* contra);
+	void setGenero(char genero);
+	void setFechaNcto(char* fecha);
+	void setNumTarjeta(int numTarj);
+};
+
+#endif /* USUARIO_H_ */
+
+
